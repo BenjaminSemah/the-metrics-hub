685fefd3ed98a56afc9f32f388b0500a031e49b2
Benjamin Semah <benjaminsemah@gmail.com>
2022-03-15 17:29:02 +0000

    Create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import { useNavigate } from "react-router-dom";
+import './Header.css';
+
+const Header = ({ value }) => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate('/snp');
+  }
+
+  return (
+    <>
+      <button type="button" className="header--btn" onclick={onClick}>
+        <div>
+          <h2>{value.name}</h2>
+        </div>
+        <div className="snp--info">
+          <p className="snp--logo">{value.symbol}</p>
+          <p className="snp--price">{value.price}</p>
+        </div>
+      </button>
+    </>
+  );
+};
+
+Header.propTypes = {
+  value: propTypes.instanceOf(Object).isRequired,
+};
+
+export default Header;
